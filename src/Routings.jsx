fe9886af4f2f4b5d9ea5c8244cc105401496e907
aaruanbu <aaruanbu001@gmail.com>
2024-03-02 21:02:04 +0530

    homepage

--- src/Routings.jsx
+++ src/Routings.jsx
@@ -3,13 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./Home";
 import { Detai } from "./Details";
 
-export const Routss=()=>{
-    return(
+export const Routss = () => {
+    return (
         <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/detls" element={<Detai />}></Route>
-        </Routes>
+            <Routes>
+                <Route path="API-fetch/" element={<Home />}></Route>
+                <Route path="/detls" element={<Detai />}></Route>
+            </Routes>
         </BrowserRouter>
     )
 }
