9445988d9fcdb5964a274979a98cc5910e927c0b
aaruanbu <aaruanbu001@gmail.com>
2024-03-02 20:57:21 +0530

    vcdd

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/API-FETCH/",
+  base: "/API-fetch/",
   plugins: [react()],
 });
